e46148b89b48a5113dafa50513a3b2d7f467f0ed
Victor Koji <vitorkoji15@gmail.com>
2019-10-08 11:30:57 -0400

    teste 2 - post

--- src/controllers/blocoNotasController.js
+++ src/controllers/blocoNotasController.js
@@ -18,8 +18,8 @@ exports.get = (req, res, next) => {
 
 exports.post = (req, res, next) => {
     const filter = [
-        req.body.titulo.substring(0,30), 
-        req.body.texto.substring(0,150)
+        req.body.titulo, 
+        req.body.texto
     ];
 
     console.log(filter);
