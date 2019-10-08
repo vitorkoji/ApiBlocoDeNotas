e08c748692a2b211357075cd1379df2e02a0c30d
Victor Koji <vitorkoji15@gmail.com>
2019-10-08 11:07:31 -0400

    version 1.0.0

--- src/controllers/blocoNotasController.js
+++ src/controllers/blocoNotasController.js
@@ -6,9 +6,12 @@ exports.getAll = (req, res, next) => {
 
 exports.get = (req, res, next) => {
     const filter = [parseInt(req.params.id)];
-    console.log(filter);
-    // const consulta = 'SELECT * FROM anotacao WHERE id_noticia= ?';
-    const consulta = 'SELECT * FROM anotacao WHERE id_noticia= $1';
+
+    //Esse jeito é utilizando mysql
+    // const consulta = 'SELECT * FROM anotacao WHERE id_anotacao= ?';
+
+    //Esse é utilizando postgre
+    const consulta = 'SELECT * FROM anotacao WHERE id_anotacao= $1';
     
     execSQLQuery(consulta, filter, res);
 };
@@ -19,19 +22,21 @@ exports.post = (req, res, next) => {
         req.body.texto.substring(0,150)
     ];
 
-    execSQLQuery(`INSERT INTO anotacao(titulo, texto) VALUES( ? , ? )`, filter, res);
+    execSQLQuery(`INSERT INTO anotacao(titulo, texto) VALUES( $1 , $2 )`, filter, res);
 };
+
 exports.patch = (req, res, next) => {
     const filter = [
         req.body.titulo.substring(0,30), 
         req.body.texto.substring(0,150),
         parseInt(req.params.id)
     ];
-    execSQLQuery(`UPDATE anotacao SET titulo= ?, texto= ? WHERE id_noticia= ?`, filter, res);
+    execSQLQuery(`UPDATE anotacao SET titulo= $1, texto= $2 WHERE id_anotacao= $3`, filter, res);
 };
+
 exports.delete = (req, res, next) => {
     const filter = [parseInt(req.params.id)];
-    execSQLQuery('DELETE FROM anotacao WHERE id_noticia= ?', filter, res);
+    execSQLQuery('DELETE FROM anotacao WHERE id_anotacao= $1', filter, res);
 };
 
 
