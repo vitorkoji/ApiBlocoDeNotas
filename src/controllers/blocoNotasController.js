21eb81792ff5b9ad9bd1e3c4cf2515db95b72004
Victor Koji <vitorkoji15@gmail.com>
2019-10-08 12:42:56 -0400

    1.0.2

--- src/controllers/blocoNotasController.js
+++ src/controllers/blocoNotasController.js
@@ -17,20 +17,18 @@ exports.get = (req, res, next) => {
 };
 
 exports.post = (req, res, next) => {
-    const titulo = req.body.titulo;
-    const texto = req.body.texto
-    const filter = [titulo, texto];
-
-    console.log(titulo);
-    console.log(filter);
+    const filter = [
+        req.body.titulo, 
+        req.body.texto
+    ];
 
     execSQLQuery(`INSERT INTO anotacao(titulo, texto) VALUES( $1 , $2 )`, filter, res);
 };
 
 exports.patch = (req, res, next) => {
     const filter = [
-        req.body.titulo.substring(0,30), 
-        req.body.texto.substring(0,150),
+        req.body.titulo, 
+        req.body.texto,
         parseInt(req.params.id)
     ];
     execSQLQuery(`UPDATE anotacao SET titulo= $1, texto= $2 WHERE id_anotacao= $3`, filter, res);
