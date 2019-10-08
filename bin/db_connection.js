ec4fb9f0b30699aad19ed1f7aa8ee6fe4246a3e1
Victor Koji <vitorkoji15@gmail.com>
2019-10-08 09:07:19 -0400

    pg add

--- bin/db_connection.js
+++ bin/db_connection.js
@@ -1,22 +1,42 @@
-module.exports = function(sqlQry, val, res){
-    var mysql = require('mysql');
-        
-    var connection = mysql.createConnection({
-        host : 'ec2-107-20-198-176.compute-1.amazonaws.com',
-        port: '5432',
-        user : 'vworkxizvmcbfi',
-        password : 'bc2224927cd29a3e5bc8082cdec4ee681ae2ef126720be754b29d3c7304defff',
-        database : 'dbsq8bpvd3037e'
-    });
+module.exports = function (sqlQry, val, res) {
+  // var mysql = require('mysql');
 
-    connection.query(sqlQry, val, function(error, results, fields){
-        if(error) 
-          res.json(error);
-        else
-          res.json(results);
-        connection.end();
-        console.log('executou!');
-    });
+  // var connection = mysql.createConnection({
+  //     host : 'ec2-107-20-198-176.compute-1.amazonaws.com',
+  //     port: '5432',
+  //     user : 'vworkxizvmcbfi',
+  //     password : 'bc2224927cd29a3e5bc8082cdec4ee681ae2ef126720be754b29d3c7304defff',
+  //     database : 'dbsq8bpvd3037e'
+  // });
 
-    return connection;
+  const Pool = require('pg').Pool
+  const pool = new Pool({
+    user: 'vworkxizvmcbfi',
+    host: 'ec2-107-20-198-176.compute-1.amazonaws.com',
+    database: 'dbsq8bpvd3037e',
+    password: 'bc2224927cd29a3e5bc8082cdec4ee681ae2ef126720be754b29d3c7304defff',
+    port: 5432,
+  })
+
+  pool.query(sqlQry, val, function (error, results, fields) {
+    if (error)
+      res.json(error);
+    else
+      res.json(results);
+    pool.end();
+    console.log('executou!');
+  });
+
+  return pool;
+
+  // connection.query(sqlQry, val, function (error, results, fields) {
+  //   if (error)
+  //     res.json(error);
+  //   else
+  //     res.json(results);
+  //   connection.end();
+  //   console.log('executou!');
+  // });
+
+  // return connection;
 }
