feebf0faf596ffffcd5508968db75fb77638977b
Victor Koji <vitorkoji15@gmail.com>
2019-10-08 08:56:42 -0400

    Conexão atualizado

--- bin/db_connection.js
+++ bin/db_connection.js
@@ -2,10 +2,11 @@ module.exports = function(sqlQry, val, res){
     var mysql = require('mysql');
         
     var connection = mysql.createConnection({
-        host : 'localhost',
-        user : 'root',
-        password : '12345678',
-        database : 'projeto'
+        host : 'ec2-107-20-198-176.compute-1.amazonaws.com',
+        port: '5432',
+        user : 'vworkxizvmcbfi',
+        password : 'bc2224927cd29a3e5bc8082cdec4ee681ae2ef126720be754b29d3c7304defff',
+        database : 'dbsq8bpvd3037e'
     });
 
     connection.query(sqlQry, val, function(error, results, fields){
